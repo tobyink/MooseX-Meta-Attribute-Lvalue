6c209a81e54ffeca5fdb32d29a407dad7fc8a9f9
Toby Inkster <mail@tobyinkster.co.uk>
2013-08-26 22:13:27 +0100

    bump version; changelog

--- lib/MooseX/LvalueAttribute/Trait/Attribute.pm
+++ lib/MooseX/LvalueAttribute/Trait/Attribute.pm
@@ -1,6 +1,6 @@
 package MooseX::LvalueAttribute::Trait::Attribute;
 
-our $VERSION   = '0.900_01';
+our $VERSION   = '0.900_02';
 our $AUTHORITY = 'cpan:TOBYINK';
 
 use Moose::Role;
--- lib/MooseX/Meta/Attribute/Lvalue.pm
+++ lib/MooseX/Meta/Attribute/Lvalue.pm
@@ -1,6 +1,6 @@
 package MooseX::Meta::Attribute::Lvalue;
 
-our $VERSION   = '0.900_01';
+our $VERSION   = '0.900_02';
 our $AUTHORITY = 'cpan:TOBYINK';
 
 use Moose::Role;
