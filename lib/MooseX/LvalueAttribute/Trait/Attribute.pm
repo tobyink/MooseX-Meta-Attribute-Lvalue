1dc1a0dc363b1aafd784d415d2b2024213b618b2
Toby Inkster <mail@tobyinkster.co.uk>
2013-08-26 17:03:41 +0100

    more docs

--- lib/MooseX/LvalueAttribute/Trait/Attribute.pm
+++ lib/MooseX/LvalueAttribute/Trait/Attribute.pm
@@ -21,3 +21,51 @@ around accessor_metaclass => sub
 };
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+MooseX::LvalueAttribute::Trait::Attribute - internals for MooseX::LvalueAttribute
+
+=head1 DESCRIPTION
+
+This attribute trait applies the L<MooseX::LvalueAttribute::Trait::Accessor>
+trait to your attribute's accessors.
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=MooseX-LvalueAttribute>.
+
+=head1 SEE ALSO
+
+L<MooseX::LvalueAttribute>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+Based on work by
+Christopher Brown, C<< <cbrown at opendatagroup.com> >>
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2013 by Toby Inkster;
+2008 by Christopher Brown.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+=cut
+
